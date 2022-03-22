3969436fe8e870b23b160195f303bf7d1884d00f
Carlos A. Moncada <carlosmoncada@gmail.com>
2022-03-21 19:48:16 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>First Code Challenge</h1>
+      <textarea 
+        name="original message" 
+        v-model="message"
+        class="textfield">
+      </textarea>
+      <button class="button" @click="toggleMesage">Show message</button>
+      <span class="error" v-if="error != ''">{{ error }}</span>
+      <textarea 
+        name="new message"
+        v-if="showMessage"
+        :value="message"
+        class="textfield"
+        readonly
+      >
+      </textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: '',
+      showMessage: false,
+      error: ''
+    }
+  },
+  methods: {
+    toggleMesage () {
+      if (this.message != '') {
+        this.showMessage = !this.showMessage
+        this.error = ''
+      }
+      else {
+        this.error = 'Message field is empty!'
+        this.showMessage = false
+      }
+    }
+  }
+}
+
+
+</script>
+
+<style>
+body {
+  background-color: whitesmoke;
+  font-family: 'Roboto', sans-serif;
+}
+
+.container {
+  display: flex;
+  width: 500px;
+  flex-direction: column;
+  padding: 40px;
+  margin:  0 auto;
+}
+
+.textfield {
+  height: 50px;
+  margin: 10px 0;
+  font-family: 'Roboto', sans-serif;
+  font-size: 14px;
+}
+
+.error {
+  font-size: 12px;
+  color: tomato;
+}
+</style>
